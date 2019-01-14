4735e41868c6d113b56fcecec76c59b493c805bd
adi0509 <41581721+adi0509@users.noreply.github.com>
2019-01-14 22:16:36 +0530

    Add files via upload

---
+++ 4th Sem/PHP prac/p2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Practical 2</title>
+</head>
+<body>
+	<form action="" method="post">
+		<input type="number" name="n">
+		<input type="submit" name="btn" >
+	</form>
+
+	<?php
+		function fact($n)
+		{
+			if($n>0)
+			{
+				$f=1;
+				for($i=$n;$i>0;$i--)
+				{
+					$f*=$i;
+				}
+				return $f;
+			}
+			return 1;
+		}
+		if(isset($_POST["btn"]))
+		{
+			$num = $_POST["n"];
+			for($i=0;$i<$num;$i++)
+			{
+				echo ($i+1);
+				if($i!=$num-1)
+				{
+					echo " * ";
+				}
+			}
+			echo " = ";
+			echo fact($num);
+		}
+	?>
+</body>
+</html>
